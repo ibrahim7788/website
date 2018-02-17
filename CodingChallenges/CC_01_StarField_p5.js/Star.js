bc072c6252860a4b37c4233ff9b9f7a7de3bbd9d
ibrahim7788 <35537628+ibrahim7788@users.noreply.github.com>
2018-02-17 08:13:31 +0200

    Update Star.js
    To show all of the stars width and height should be divided by 2 as we are translating to the center of the screen

--- CodingChallenges/CC_01_StarField_p5.js/Star.js
+++ CodingChallenges/CC_01_StarField_p5.js/Star.js
@@ -4,8 +4,8 @@
 // Code for: https://youtu.be/17WoOqgXsRM
 
 function Star() {
-  this.x = random(-width, width);
-  this.y = random(-height, height);
+  this.x = random(-width/2, width/2);
+  this.y = random(-height/2, height/2);
   this.z = random(width);
   this.pz = this.z;
 
@@ -13,8 +13,8 @@ function Star() {
     this.z = this.z - speed;
     if (this.z < 1) {
       this.z = width;
-      this.x = random(-width, width);
-      this.y = random(-height, height);
+      this.x = random(-width/2, width/2);
+      this.y = random(-height/2, height/2);
       this.pz = this.z;
     }
   }
@@ -23,14 +23,14 @@ function Star() {
     fill(255);
     noStroke();
 
-    var sx = map(this.x / this.z, 0, 1, 0, width);
-    var sy = map(this.y / this.z, 0, 1, 0, height);
+    var sx = map(this.x / this.z, 0, 1, 0, width/2);
+    var sy = map(this.y / this.z, 0, 1, 0, height/2);
 
     var r = map(this.z, 0, width, 16, 0);
     ellipse(sx, sy, r, r);
 
-    var px = map(this.x / this.pz, 0, 1, 0, width);
-    var py = map(this.y / this.pz, 0, 1, 0, height);
+    var px = map(this.x / this.pz, 0, 1, 0, width/2);
+    var py = map(this.y / this.pz, 0, 1, 0, height/2);
 
     this.pz = this.z;
 
